a70442d236c633d1cb2eac4563e62789116297b6
Nick Krupa <nick@workingdev.com>
2022-11-01 19:28:12 -0500

    basic controller

---
+++ app/controllers/ai_controller.rb
@@ -0,0 +1,5 @@
+class AiController < ApplicationController
+  
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'pages/vcard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resource :ai, controller: :ai
+
   # Defines the root path route ("/")
   root "pages#home"
 end
